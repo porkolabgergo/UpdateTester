7f52bd6ad376ca1423e4fe9f076b609bcf5fda5a
porkolabgergo <41578426+porkolabgergo@users.noreply.github.com>
2022-09-21 13:08:38 +0200

    Updater init

--- UpdateTester/App.xaml.cs
+++ UpdateTester/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Squirrel;
 
 namespace UpdateTester
 {
@@ -13,5 +14,13 @@ namespace UpdateTester
     /// </summary>
     public partial class App : Application
     {
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            using (var mgr = new UpdateManager("C:\\Users\\porkg\\OneDrive\\Documents\\GitHub\\UpdateTester\\Releases"))
+            {
+                await mgr.UpdateApp();
+            }
+        }
     }
 }
